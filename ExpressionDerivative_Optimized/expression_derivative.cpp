1a67d615e6267cbff4cc13bcb5ecd1611b734694
Takkasila Saichol <takkasila@outlook.com>
2017-02-28 20:32:58 +0700

    5. Expression derivative

---
+++ ExpressionDerivative_Optimized/expression_derivative.cpp
@@ -0,0 +1,451 @@
+#include <iostream>
+#include <string>
+#include <assert.h>
+#include <stdio.h>
+#include <math.h>
+
+using namespace std;
+
+char ch;
+int val;
+int sym;
+char variable;
+
+enum Symbol
+{
+	Plus, Minus, Times, Divide, Mod, Power, LParen, RParen, Number, EndLine, Illegal, Var
+};
+
+class Node
+{
+public:
+	int kind;
+	int val;
+	Node *left;
+	Node *right;
+	
+	Node()
+	{
+		kind = Illegal;
+		val = 0;
+		left = NULL;
+		right = NULL;
+	}
+	
+	Node & operator = (Node *other)
+	{
+		kind = other->kind;
+		val = other->val;
+		left = other->left;
+		right = other->right;
+	}
+
+	Node *duplicate()
+	{
+		Node *newRoot = new Node();
+		newRoot->kind = kind;
+		newRoot->val = val;
+		if (left != NULL)
+			newRoot->left = left->duplicate();
+		if (right != NULL)
+			newRoot->right = right->duplicate();
+		return newRoot;
+	}
+	
+	void pow2()
+	{
+		Node *tempLeft = this->duplicate();
+		Node *tempRight = this->duplicate();
+		kind = Times;
+		val = 0;
+		left = tempLeft;
+		right = tempRight;
+	}
+
+	void diff()
+	{
+		switch (kind)
+		{
+		case Plus:
+		case Minus:
+			left->diff();
+			right->diff();
+			break;
+		case Times:
+		{
+			Node* copyLeft = left->duplicate();
+			Node* copyLeftDiff = left->duplicate();
+			copyLeftDiff->diff();
+			Node* copyRight = right->duplicate();
+			Node* copyRightDiff = right->duplicate();
+			copyRightDiff->diff();
+
+			Node* leftTerm = new Node();
+			leftTerm->kind = Times;
+			leftTerm->left = copyLeft;
+			leftTerm->right = copyRightDiff;
+
+			Node* rightTerm = new Node();
+			rightTerm->kind = Times;
+			rightTerm->left = copyRight;
+			rightTerm->right = copyLeftDiff;
+
+			kind = Plus;
+			left = leftTerm;
+			right = rightTerm;
+			break;
+		}
+		case Divide:
+		{
+			Node* copyLeft = left->duplicate();
+			Node* copyLeftDiff = left->duplicate();
+			copyLeftDiff->diff();
+			Node* copyRight = right->duplicate();
+			Node* copyRightDiff = right->duplicate();
+			copyRightDiff->diff();
+
+			Node* leftTerm = new Node();
+			leftTerm->kind = Times;
+			leftTerm->left = copyRight;
+			leftTerm->right = copyLeftDiff;
+
+			Node* rightTerm = new Node();
+			rightTerm->kind = Times;
+			rightTerm->left = copyLeft;
+			rightTerm->right = copyRightDiff;
+
+			Node* midTerm = new Node();
+			midTerm->kind = Minus;
+			midTerm->left = leftTerm;
+			midTerm->right = rightTerm;
+
+			Node *bottomPow2 = right->duplicate();
+			bottomPow2->pow2();
+
+			kind = Divide;
+			left = midTerm;
+			right = bottomPow2;
+			break;
+		}
+		case Power:
+		{
+			//x^y = y*x^(y-1)*x'
+			//(y-1)
+			Node *yTemp1 = right->duplicate();
+			Node *one = new Node();
+			one->kind = Number;
+			one->val = 1;
+			Node *termYMinus1 = new Node();
+			termYMinus1->kind = Minus;
+			termYMinus1->left = yTemp1;
+			termYMinus1->right = one;
+			//x^(y-1)
+			Node *xTemp1 = left->duplicate();
+			Node *termxPowYMinus = new Node();
+			termxPowYMinus->kind = Power;
+			termxPowYMinus->left = xTemp1;
+			termxPowYMinus->right = termYMinus1;
+			//y*x^(y-1)
+			Node *yTemp2 = right->duplicate();
+			Node *termYTimesXPow = new Node();
+			termYTimesXPow->kind = Times;
+			termYTimesXPow->left = yTemp2;
+			termYTimesXPow->right = termxPowYMinus;
+			//x'
+			Node *xDiff = left->duplicate();
+			xDiff->diff();
+			//x^y = y*x^(y-1)*x'
+			kind = Times;
+			left = termYTimesXPow;
+			right = xDiff;
+			break;
+		}
+		case Number:
+			val = 0;
+			break;
+		case Var:
+			kind = Number;
+			val = 1;
+			break;
+		}
+	}
+};
+
+void getNumber()
+{
+	// digit, { digit };
+	val = 0;
+	while (('0' <= ch) && (ch <= '9'))
+	{
+		// Char to int
+		val = val * 10 + ch - '0';
+		ch = getchar();
+	}
+}
+
+int getSymbol()
+{
+	int sym;
+	while ((ch != EOF) && (ch <= ' ') && (ch != '\n'))
+		ch = getchar();
+	switch (ch)
+	{
+	case '\n':
+		sym = EndLine;
+		break;
+	case '+':
+		sym = Plus;
+		ch = getchar();
+		break;
+	case '-':
+		sym = Minus;
+		ch = getchar();
+		break;
+	case '*':
+		sym = Times;
+		ch = getchar();
+		break;
+	case '/':
+		sym = Divide;
+		ch = getchar();
+		break;
+	case '%':
+		sym = Mod;
+		ch = getchar();
+		break;
+	case '^':
+		sym = Power;
+		ch = getchar();
+		break;
+	case '(':
+		sym = LParen;
+		ch = getchar();
+		break;
+	case ')':
+		sym = RParen;
+		ch = getchar();
+		break;
+		// digit = "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9";
+	case '0': case '1': case '2': case '3': case '4': case '5': case '6': case '7': case '8': case '9':
+		sym = Number;
+		getNumber();
+		break;
+	default:
+		if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))
+		{
+			sym = Var;
+			variable = ch;
+			ch = getchar();
+		}
+		else
+			sym = Illegal;
+	}
+	return sym;
+}
+
+int expression(Node *node);
+
+int factor(Node *node)
+{
+	// factor = constant | "(" , expression , ")" | Var;
+	int res = 0;
+	assert(sym == Number || sym == LParen || sym == Var);
+	if (sym == Number)
+	{
+		node->kind = sym;
+		node->val = val;
+		res = val;
+		sym = getSymbol();
+	}
+	else if (sym == LParen)
+	{
+		sym = getSymbol();
+		res = expression(node);
+		assert(sym == RParen);
+		sym = getSymbol();
+	}
+	else
+	{
+		node->kind = sym;
+		node->val = variable;
+		sym = getSymbol();
+	}
+	return res;
+}
+
+int preFactor(Node *node)
+{
+	// preFactor = factor , { "^" , factor };
+	Node *leftNode = new Node();
+	int res = 0;
+	res = factor(leftNode);
+
+	*node = *leftNode;
+	Node *tempLeftNode = new Node();
+	tempLeftNode->left = leftNode;
+	while (sym == Power)
+	{
+		Node *midNode = new Node();
+		midNode->kind = sym;
+		Node *rightNode = new Node();
+		sym = getSymbol();
+		res = pow(res, factor(rightNode));
+
+		midNode->left = tempLeftNode->left;
+		midNode->right = rightNode;
+		tempLeftNode->left = midNode;
+		*node = *midNode;
+	}
+	return res;
+}
+
+int term(Node *node)
+{
+	//term = preFactor, { ("*" | "/" | "%"), preFactor };
+	Node *leftNode = new Node();
+	int res = 0;
+	res = preFactor(leftNode);
+
+	*node = *leftNode;
+	Node *tempLeftNode = new Node();
+	tempLeftNode->left = leftNode;
+	while (sym == Times || sym == Divide || sym == Mod)
+	{
+		Node *midNode = new Node();
+		Node *rightNode = new Node();
+		switch (sym)
+		{
+		case Times:
+			midNode->kind = sym;
+			sym = getSymbol();
+			res *= preFactor(rightNode);
+			break;
+		case Divide:
+			midNode->kind = sym;
+			sym = getSymbol();
+			res /= preFactor(rightNode);
+			break;
+		case Mod:
+			midNode->kind = sym;
+			sym = getSymbol();
+			res %= preFactor(rightNode);
+			break;
+		}
+		midNode->left = tempLeftNode->left;
+		midNode->right = rightNode;
+		tempLeftNode->left = midNode;
+		*node = *midNode;
+	}
+	return res;
+}
+
+int expression(Node *node)
+{
+	//expression = ["+" | "-"], term, { ("+" | "-"), term };
+	Node *leftNode = new Node();
+	int positivity = 1;
+	int res = 0;
+
+	switch (sym)
+	{
+	case Plus:
+		positivity = 1;
+		sym = getSymbol();
+		break;
+	case Minus:
+		positivity = -1;
+		sym = getSymbol();
+		break;
+	}
+
+	res = positivity * term(leftNode);
+	*node = *leftNode;
+	Node *tempLeftNode = new Node();
+	tempLeftNode->left = leftNode;
+	while (sym == Plus || sym == Minus)
+	{
+		switch (sym)
+		{
+		case Plus:
+			positivity = 1;
+			break;
+		case Minus:
+			positivity = -1;
+			break;
+		}
+		Node *midNode = new Node();
+		midNode->kind = sym;
+
+		Node *rightNode = new Node();
+		sym = getSymbol();
+		res += positivity * term(rightNode);
+
+		midNode->left = tempLeftNode->left;
+		midNode->right = rightNode;
+
+		tempLeftNode->left = midNode;
+		*node = *midNode;
+	}
+	return res;
+}
+
+void printExpressionTree(Node *node, int level)
+{
+	if (node->kind == Illegal)
+		return;
+
+	if (node->right != NULL)
+		printExpressionTree(node->right, level + 1);
+
+	for (int i = 0; i < level; i++)
+		cout << "    ";
+	switch (node->kind)
+	{
+	case Plus:
+		cout << "+" << endl;
+		break;
+	case Minus:
+		cout << "-" << endl;
+		break;
+	case Times:
+		cout << "*" << endl;
+		break;
+	case Divide:
+		cout << "/" << endl;
+		break;
+	case Mod:
+		cout << "%" << endl;
+		break;
+	case Power:
+		cout << "^" << endl;
+		break;
+	case Number:
+		cout << node->val << endl;
+		break;
+	case Var:
+		cout << (char)node->val << endl;
+		break;
+	}
+	if (node->left != NULL)
+		printExpressionTree(node->left, level + 1);
+}
+
+int main()
+{
+	Node *root = new Node();
+	cout << "Input expression: ";
+	ch = getchar();
+
+	sym = getSymbol();
+	expression(root);
+
+	assert(sym == EndLine);
+	cout << "Succesfully parsed an expression." << endl;
+	cout << "Expression Tree: " << endl;
+	printExpressionTree(root, 0);
+	root->diff();
+	cout << "Diff Expression Tree: " << endl;
+	printExpressionTree(root, 0);
+	getchar();
+	return 0;
+}
